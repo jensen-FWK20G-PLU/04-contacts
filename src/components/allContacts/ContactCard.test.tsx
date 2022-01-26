6c2fa685d7c30603253a4622fd63ea8c2f3127b3
David Andersson <david.andersson@zocom.se>
2022-01-26 11:40:46 +0100

    Contact card uses Lifting state up now

--- src/components/allContacts/ContactCard.test.tsx
+++ src/components/allContacts/ContactCard.test.tsx
@@ -1,21 +1,26 @@
 import { render, screen/*, queryByText, within*/ } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Contact } from '../../models/Contact'
 import ContactCard from './ContactCard'
 
 describe('ContactCard component', () => {
 	let testContact: Contact;
+	let mockChangeContact: jest.Mock; //(newContact: Contact) => void;
+	let jsxCard: any;  // TODO: replace any with the correct data type
 	
 	beforeEach(() => {
 		testContact = createContact();
+		mockChangeContact = jest.fn()
+		jsxCard = <ContactCard contact={testContact} changeContact={mockChangeContact} />
 	})
 
 
 	it('renders without crashing', () => {
-		render( <ContactCard contact={testContact} /> )
+		render( jsxCard )
 	})
 
 	it('renders a pen button (for editing a contact)', () => {
-		render( <ContactCard contact={testContact} /> )
+		render( jsxCard )
 
 		// There is only one button in the component (for now)
 		const button = screen.getByRole('button')
@@ -24,18 +29,44 @@ describe('ContactCard component', () => {
 	})
 
 	it('shows no input fields initially', () => {
-		render( <ContactCard contact={testContact} /> )
+		render( jsxCard )
 
 		const maybeInputs = screen.queryAllByRole('textbox')
 
 		expect(maybeInputs.length).toBe(0)
 	})
 
+	it('shows input fields after click on pen', () => {
+		render( jsxCard )
 
-	// shows input fields after click on pen
+		const button = screen.getByRole('button')
+		userEvent.click(button)
 
+		const maybeInputs = screen.queryAllByRole('textbox')
 
-	// vad ska hända när man ändrar??
+		expect(maybeInputs.length).toBeGreaterThan(0)
+	})
+
+	// In actuality, we test that changeContact is called with the correct new name
+	it('shows the updated value after editing name', () => {
+		render( jsxCard )
+		const newName = 'Leifi Plomeros'
+		
+		const button = screen.getByRole('button')
+		userEvent.click(button)
+
+		const nameInput = screen.getByDisplayValue(testContact.name)
+		userEvent.type(nameInput, '{selectall}{backspace}' + newName)
+		userEvent.click(button)
+		// changeContact should be called once with the new Contact object now
+
+		expect(mockChangeContact.mock.calls.length).toBe(1)
+		const firstCall = mockChangeContact.mock.calls[0]
+		const newContact = firstCall[0]
+		expect(newContact.name).toBe(newName)
+	})
+
+	// TODO: test case that verifies that the contact data is rendered
 })
 
 function createContact(): Contact {
--- src/components/allContacts/ContactCard.tsx
+++ src/components/allContacts/ContactCard.tsx
@@ -1,20 +1,41 @@
+import { useState, ChangeEvent } from 'react'
 import { Contact } from '../../models/Contact'
 
 interface Props {
 	contact: Contact;
+	changeContact: (newContact: Contact) => void;
 }
 
+// Alternativ syntax - destructuring två gånger, för att slippa variabeln "contact"
 // const ContactCard = ({ contact: { id, name, phoneNumbers } }: Props) => {
-const ContactCard = ({ contact }: Props) => {
-	// props.contact.name osv.
+const ContactCard = ({ contact, changeContact }: Props) => {
+	// State
+	const [isEditing, setIsEditing] = useState<boolean>(false)
+	const [name, setName] = useState<string>(contact.name)
+
+	// Event functions
+	const editClick = () => {
+		if( isEditing ) {
+			changeContact({ ...contact, name: name })
+		}
+		setIsEditing(!isEditing)
+	}
+
+	const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)
+	
+
+	// JSX
 	return (
 		<li data-testid={'contact' + contact.id}>
-			<h3> {contact.name} </h3>
+			<h3> {isEditing
+				? <input type="text" value={name} onChange={handleNameChange} />
+				: contact.name}
+			</h3>
 			{contact.phoneNumbers.map(phone => (
 				<div key={phone}> {phone} </div>
 			))}
 			<div className="star">
-				<div role="button">🖊️</div>
+				<div role="button" onClick={editClick}>🖊️</div>
 			</div>
 		</li>
 	)
